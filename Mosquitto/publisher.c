11fc5c044bba5d52979612fb8a4dfd043f4910c7
Central Hub Station <spectre.battlezone@gmail.com>
2018-03-03 15:54:08 -0700

    For DWM testing with mosquitto

--- Mosquitto/publisher.c
+++ Mosquitto/publisher.c
@@ -9,14 +9,35 @@
 // Server connection parameters
 #define MQTT_HOSTNAME "129.123.5.197"
 #define MQTT_NAME "Server_Publisher"
+#define MQTT_NAME_SUB "Server_Subscriber"
 #define MQTT_PORT 1883
 #define MQTT_TOPIC "location_sync"
+#define MQTT_TOPIC_TAG "location_tag"
+
+static struct mosquitto *mosq; 
+
+void message_callback(struct mosquitto *mosq, void *obj, const struct mosquitto_message *message)
+{
+	bool match = 0;
+	printf("got message '%.*s' for topic '%s'\n", message->payloadlen, (char*) message->payload, message->topic);
+
+	mosquitto_topic_matches_sub(MQTT_TOPIC_TAG, message->topic, &match);
+	if (match) {
+		printf("got message for %s topic\n", MQTT_TOPIC);
+	}
+}
 
 int main(){
     
     mosquitto_lib_init();
-    struct mosquitto *mosq = mosquitto_new(MQTT_NAME, true, NULL);
+    mosq = mosquitto_new(MQTT_NAME, true, NULL);
     if(!mosq){
+        fprintf(stderr, "Could not initialize mosquitto library. Quitting\n");
+        exit(-1);
+    }
+
+    struct mosquitto *mosq_sub = mosquitto_new(MQTT_NAME_SUB, true, NULL);
+    if(!mosq_sub){
         fprintf(stderr, "Could not initialize mosquitto library. Quitting\n");
         exit(-1);
     }
@@ -26,29 +47,63 @@ int main(){
         exit(-2);
     }
 
+    if(mosquitto_connect(mosq_sub, MQTT_HOSTNAME, MQTT_PORT, 0)){
+        fprintf(stderr, "Could not connect to mosquitto broker. Quitting\n");
+        exit(-2);
+    }
+
+    mosquitto_message_callback_set(mosq_sub, message_callback);
+    mosquitto_subscribe(mosq_sub, NULL, MQTT_TOPIC_TAG, 1);
+
     char buf[7];
 
     while(1){
+        int ret = mosquitto_loop(mosq_sub, 250, 1); //different thread?
+        if(ret){
+		    fprintf(stderr, "Connection error. Reconnecting...\n");
+		    sleep(1);
+		    mosquitto_reconnect(mosq_sub);
+	    }
         sprintf(buf, "Round%i", 1);
         if(mosquitto_publish(mosq, NULL, MQTT_TOPIC, strlen(buf), buf, 0, false)){
             fprintf(stderr, "Could not publish to broker. Quitting\n");
             exit(-3);
         }
-        sleep(1);
+        ret = mosquitto_loop(mosq_sub, 250, 1); //different thread?
+        if(ret){
+		    fprintf(stderr, "Connection error. Reconnecting...\n");
+		    sleep(1);
+		    mosquitto_reconnect(mosq_sub);
+	    }
+        usleep(200000);
         
         sprintf(buf, "Round%i", 2);
         if(mosquitto_publish(mosq, NULL, MQTT_TOPIC, strlen(buf), buf, 0, false)){
             fprintf(stderr, "Could not publish to broker. Quitting\n");
             exit(-3);
         }
-        sleep(1);
+        ret = mosquitto_loop(mosq_sub, 250, 1); //different thread?
+        if(ret){
+		    fprintf(stderr, "Connection error. Reconnecting...\n");
+		    sleep(1);
+		    mosquitto_reconnect(mosq_sub);
+	    }
+        usleep(200000);
 
-        sprintf(buf, "Round%i", 3);
-        if(mosquitto_publish(mosq, NULL, MQTT_TOPIC, strlen(buf), buf, 0, false)){
-            fprintf(stderr, "Could not publish to broker. Quitting\n");
-            exit(-3);
-        }
-        sleep(1);
+        //add when 6 tags + anchors are used
+
+        // sprintf(buf, "Round%i", 3);
+        // if(mosquitto_publish(mosq, NULL, MQTT_TOPIC, strlen(buf), buf, 0, false)){
+        //     fprintf(stderr, "Could not publish to broker. Quitting\n");
+        //     exit(-3);
+        // }
+        // ret = mosquitto_loop(mosq_sub, 250, 1); //different thread?
+        // if(ret){
+		//     fprintf(stderr, "Connection error. Reconnecting...\n");
+		//     sleep(1);
+		//     mosquitto_reconnect(mosq_sub);
+	    // }
+        // usleep(500000);
     }
 
     mosquitto_disconnect (mosq);
