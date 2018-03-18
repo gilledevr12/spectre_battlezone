7a603806d90a8fffc215720d8d136b236f996449
Devin <devingillespie612@gmail.com>
2018-03-18 15:25:58 -0600

    trying automatic distance recording

--- Mosquitto/publisher-2.c
+++ Mosquitto/publisher-2.c
@@ -14,6 +14,7 @@
 #define MQTT_PORT 1883
 #define MQTT_TOPIC "location_sync"
 #define MQTT_TOPIC_TAG "location_tag"
+#define MQTT_TOPIC_INIT "location_init"
 
 static struct mosquitto *mosq;
 static double a1_x;
@@ -25,6 +26,9 @@ static double a3_y;
 static double a1_const;
 static double a2_const;
 static double a3_const;
+static double dist_buf[10];
+static int index;
+static bool access;
 
 void message_callback(struct mosquitto *mosq, void *obj, const struct mosquitto_message *message)
 {
@@ -79,8 +83,88 @@ void message_callback(struct mosquitto *mosq, void *obj, const struct mosquitto_
     }
 }
 
+void message_callback_init(struct mosquitto *mosq, void *obj, const struct mosquitto_message *message)
+{
+    bool match = 0;
+    index += 1;
+    //printf("got message '%.*s' for topic '%s'\n", message->payloadlen, (char*) message->payload, message->topic);
+    mosquitto_topic_matches_sub(MQTT_TOPIC_INIT, message->topic, &match);
+    if (match) {
+        double d1;
+        int tag = 0;
+        const char s[2] = " ";
+        char *token;
+        /* get the first token */
+        token = strtok((char*) message->payload, s);
+        int cnt = 0;
+        bool setTag = false;
+        /* walk through other tokens */
+        while( token != NULL ) {
+            //printf( "%s\n", token);
+            if (strcmp(token,"m") != 0) cnt += 1;
+            if (setTag){
+                tag = atoi(token);
+                setTag = false;
+            }
+            if (strcmp(token,"Tag:") == 0) setTag = true;
+            if (!(cnt % 6)) {
+                if ((cnt / 6) == 1){
+                    d1 = pow(atof(token),2.0);
+                }
+            }
+            token = strtok(NULL, s);
+        }
+
+        dist_buf[index] = d1;
+
+        if (index == 9) access = true;
+
+        //printf("%.*s\n", message->payloadlen, (char*) message->payload);
+        //printf("got message for %s topic\n", MQTT_TOPIC);
+    }
+}
+
+void getDist(struct mosquitto *mosq, struct mosquitto *mosq_sub, int anchor, char axis){
+    size_t BLEN = 16;
+    char* buf = 0;
+    int tag = 1;
+    //prep in your spot
+    printf("Position the tag please for Anchor %d %c position \n", anchor, axis);
+    usleep(10000000);
+    int ret = mosquitto_loop(mosq_sub, 250, 1); //different thread?
+    index = -1;
+    for(int i = 0; i < 10; i++) {
+        if(ret){
+            fprintf(stderr, "Connection error. Reconnecting...\n");
+            sleep(1);
+            mosquitto_reconnect(mosq_sub);
+        }
+        sprintf(buf, "Anchor%d Tag%d", anchor, tag);
+        if(mosquitto_publish(mosq, NULL, MQTT_TOPIC_INIT, strlen(buf), buf, 0, false)){
+            fprintf(stderr, "Could not publish to broker. Quitting\n");
+            exit(-3);
+        }
+        ret = mosquitto_loop(mosq_sub, 250, 1); //different thread?
+        usleep(40000);
+    }
+//    usleep(1000000);
+    //wait to access buffer
+    while (!access) {};
+    double total;
+    for (int i = 0; i < 10; i++){
+        total += dist_buf[i];
+    }
+    total = total/10;
+    access = false;
+    printf("Anchor %d %c position in meters (2 decimal points): ", anchor, axis);
+//    getline(&buf, &BLEN, stdin);
+    return total;
+}
+
 int main(){
 
+    index = 0;
+    access = false;
     mosquitto_lib_init();
     mosq = mosquitto_new(MQTT_NAME, true, NULL);
     if(!mosq){
@@ -105,40 +189,59 @@ int main(){
     }
 
     mosquitto_message_callback_set(mosq_sub, message_callback);
+    mosquitto_message_callback_set(mosq_sub, message_callback_init);
     mosquitto_subscribe(mosq_sub, NULL, MQTT_TOPIC_TAG, 1);
+
+    double a1_x_dist = getDist(mosq, mosq_sub, 1, 'x');
+    a1_x = a1_x_dist * (-1) * 2;
+    double a1_y_dist = getDist(mosq, mosq_sub, 1, 'y');
+    a1_y = a1_y_dist * (-1) * 2;
+    a1_const = pow(a1_x_dist,2) + pow(a1_y_dist,2);
+
+    double a2_x_dist = getDist(mosq, mosq_sub, 2, 'x');
+    a2_x = a2_x_dist * (-1) * 2;
+    double a2_y_dist = getDist(mosq, mosq_sub, 2, 'y');
+    a2_y = a2_y_dist * (-1) * 2;
+    a2_const = pow(a2_x_dist,2) + pow(a2_y_dist,2);
+
+    double a3_x_dist = getDist(mosq, mosq_sub, 3, 'x');
+    a3_x = a3_x_dist * (-1) * 2;
+    double a3_y_dist = getDist(mosq, mosq_sub, 3, 'y');
+    a3_y = a3_y_dist * (-1) * 2;
+    a3_const = pow(a3_x_dist,2) + pow(a3_y_dist,2);
 
     size_t BLEN = 16;
     char* buf = 0;
     int tagCnt = 1;
     int anchorCnt = 1;
 
-    printf("Anchor 1 x position in meters (2 decimal points): ");
-    getline(&buf, &BLEN, stdin);
-    double a1_x_dist = atof(buf);
-    a1_x = a1_x_dist * (-1) * 2;
-    printf("Anchor 1 y position in meters (2 decimal points): ");
-    getline(&buf, &BLEN, stdin);
-    double a1_y_dist = atof(buf);
-    a1_y = a1_y_dist * (-1) * 2;
-    a1_const = pow(a1_x_dist,2) + pow(a1_y_dist,2);
-    printf("Anchor 2 x position in meters (2 decimal points): ");
-    getline(&buf, &BLEN, stdin);
-    double a2_x_dist = atof(buf);
-    a2_x = a2_x_dist * (-1) * 2;
-    printf("Anchor 2 y position in meters (2 decimal points): ");
-    getline(&buf, &BLEN, stdin);
-    double a2_y_dist = atof(buf);
-    a2_y = a2_y_dist * (-1) * 2;
-    a2_const = pow(a2_x_dist,2) + pow(a2_y_dist,2);
-    printf("Anchor 3 x position in meters (2 decimal points): ");
-    getline(&buf, &BLEN, stdin);
-    double a3_x_dist = atof(buf);
-    a3_x = a3_x_dist * (-1) * 2;
-    printf("Anchor 3 y position in meters (2 decimal points): ");
-    getline(&buf, &BLEN, stdin);
-    double a3_y_dist = atof(buf);
-    a3_y = a3_y_dist * (-1) * 2;
-    a3_const = pow(a3_x_dist,2) + pow(a3_y_dist,2);
+//    printf("Anchor 1 x position in meters (2 decimal points): ");
+//    getline(&buf, &BLEN, stdin);
+//    double a1_x_dist = atof(buf);
+//    a1_x = a1_x_dist * (-1) * 2;
+//    printf("Anchor 1 y position in meters (2 decimal points): ");
+//    getline(&buf, &BLEN, stdin);
+//    double a1_y_dist = atof(buf);
+//    a1_y = a1_y_dist * (-1) * 2;
+//    a1_const = pow(a1_x_dist,2) + pow(a1_y_dist,2);
+//    printf("Anchor 2 x position in meters (2 decimal points): ");
+//    getline(&buf, &BLEN, stdin);
+//    double a2_x_dist = atof(buf);
+//    a2_x = a2_x_dist * (-1) * 2;
+//    printf("Anchor 2 y position in meters (2 decimal points): ");
+//    getline(&buf, &BLEN, stdin);
+//    double a2_y_dist = atof(buf);
+//    a2_y = a2_y_dist * (-1) * 2;
+//    a2_const = pow(a2_x_dist,2) + pow(a2_y_dist,2);
+//    printf("Anchor 3 x position in meters (2 decimal points): ");
+//    getline(&buf, &BLEN, stdin);
+//    double a3_x_dist = atof(buf);
+//    a3_x = a3_x_dist * (-1) * 2;
+//    printf("Anchor 3 y position in meters (2 decimal points): ");
+//    getline(&buf, &BLEN, stdin);
+//    double a3_y_dist = atof(buf);
+//    a3_y = a3_y_dist * (-1) * 2;
+//    a3_const = pow(a3_x_dist,2) + pow(a3_y_dist,2);
 
     while(1){
         int ret = mosquitto_loop(mosq_sub, 250, 1); //different thread?
