84bb38abd12de10bd0778436fa410b907bb7166d
Taylor Spencer <tayspen72@gmail.com>
2018-04-19 21:44:52 -0600

    Modified tag config to disable display blanking

--- rpi_config/tag_config.sh
+++ rpi_config/tag_config.sh
@@ -12,3 +12,7 @@ cp launch_spectre.sh 	/home/pi/.
 # Replace the tag ID in the .bashrc for laser-brains method launch
 sed -i "s/$LASER_PROGRAM &/$LASER_PROGRAM $TAG_ID&/" /home/pi/launch_spectre.sh
 
+#Endure the displays will not timeout or powerdown
+sed -i "s/BLANK_TIME.*/BLANK_TIME=0/" /etc/kdb/config
+sed -i "s/POWERDOWN_TIME.*/POWERDOWN_TIME=0/" /etc/kdb/config
+/etc/init.d/kdb restart
