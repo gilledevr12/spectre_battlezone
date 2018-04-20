77f6c9af5cd468a2178a3783f2241a5ed4603b57
Evan <egelion@protonmail.com>
2018-04-19 19:24:15 -0600

    added player objects and getTrajectory function

--- js_server/gameloop.js
+++ js_server/gameloop.js
@@ -154,3 +154,145 @@ function init(http) {
 }
 
 module.exports.init = init;
+
+function makePlayer(spec){
+    let that = {};
+
+    let stats = {
+        id: spec.id,
+        alive: true,
+        health: 100,
+        armor: 0,
+    }
+
+    let weapons = [];
+    weapons.push("pea_shooter");
+
+    let position = {
+        x: spec.position.x,
+        y: spec.position.y
+    }
+    
+    let direction = spec.direction;
+
+    return that;
+}
+
+function add_weapon(player, weapon){
+    player.weapons.push(weapon);
+}
+
+function set_direction(player, spec){
+    player.direction = spec.direction;
+}
+
+function get_direction(player){
+    return player.direction;
+}
+
+function set_position(player, spec){
+    player.position.x = spec.position.x;
+    player.position.y = spec.position.y;
+}
+
+function get_postion(player){
+    return player.position;
+}
+
+function player_hit(distance, weapon){
+    console.log("Player was hit!");
+    let power = weapon.power;
+    if(weapon === 'shotgun'){
+        power = weapon.power - distance;
+    }
+    if(player.armor > 0){
+        let remainder = 0;
+        if(player.armor - power < 0){
+            player.armor = 0;
+            remainder = -1 * (player.armor - power); 
+            player.health -= remainder;
+        }
+    }
+    else{
+        player.health -= power;
+    }
+    if(player.health <= 0){
+        player.alive = false;
+    }
+}
+
+function isInTrajectory(id1, id2, me, myTheta, you){
+    console.log(id1 + ": " + me.x + "," me.y + " firing at " + id2 + ": " + you.x + "," + you.y + " with trajectory: " + myTheta);
+    tempTheta = Math.atan2(you.y - me.y, you.x - me.x) * 180 / Math.PI;
+    console.log("calcd theta: " + tempTheta);
+    tempDistance = Math.sqrt((you.y - me.y)*(you.y - me.y) + (you.x - me.x)*(you.x - me.x));
+    thetaTolerance = tempDistance * 0.25;
+    console.log("distance: " + tempDistance + " tolerance " + thetaTolerance);
+    if((tempTheta < myTheta + thetaTolerance) && (temp_theta > my_theta - theta_tolerance))
+        return 1;
+    else
+        return 0;
+}
+
+function testTrajectory(){
+    let spec1 = { position: { x: 2,  y: 2 }, direction:   55 }
+    let spec2 = { position: { x: 17, y: 4 }, direction: -200 }
+    let spec3 = { position: { x: 2,  y: 2 }, direction:   55 }
+    let p1 = makePlayer(spec1);
+    let p2 = makePlayer(spec2);
+    let p3 = makePlayer(spec3);
+
+    let ret = isInTrajectory(1, 3, p1, 55.0, p3);
+    if(ret)
+        console.log("HIT!");
+    else
+        console.log("MISS!");
+
+    let ret = isInTrajectory(1, 3, p3, -200.0, p1);
+    if(ret)
+        console.log("HIT!");
+    else
+        console.log("MISS!");
+}
+
+
+/*#define WEAPON_COUNT    4
+
+//WEAPON CLASS
+#define PEA_SHOOTER     0
+#define SHOTGUN         1
+#define ASSAULT_RIFLE   2
+#define SPECTRE_RIFLE   3
+
+//RELOAD DELAY, FIRE DELAY
+#define FAST            0
+#define AVERAGE         1
+#define SLOW            2
+
+//FIRE WEIGHT (multiplier)
+#define VARIES          0
+#define LOW             0.5
+#define MID             1
+#define HIGH            2
+
+typedef struct {
+  char CLASS;
+  char RELOAD_DELAY;
+  char FIRE_DELAY;
+  char FIRE_WEIGHT;
+  char CLIP_SIZE;
+} WEAPON;
+
+#endif
+#ifndef INVENTORY_H
+#define INVENTORY_H
+#include "rifle_variables.h"
+
+typedef struct {
+    WEAPON WEAPON_NAME;
+    bool EQUIPPED;
+    char AMMO_REMAINING;
+    char MAX_AMMO;
+} INVENTORY_ITEM;
+*/
+
