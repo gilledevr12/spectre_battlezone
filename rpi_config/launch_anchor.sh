ff71b9b58ee6a9392f57597dd78caacabd7b74da
Central Hub Station <spectre.battlezone@gmail.com>
2018-04-30 16:48:36 -0600

    dumb

--- rpi_config/launch_anchor.sh
+++ rpi_config/launch_anchor.sh
@@ -4,10 +4,10 @@ ANCHOR_PROGRAM=$SPECTRE_DIR/dwm1000/2-init
 
 # Ensure recent config files are stored correctly
 
-if [ -f $ANCHOR_PROGRAM ]
+if [ ! -f $ANCHOR_PROGRAM ]
 then
 	#Program does not exist. Re-run 'make'
-	cd $SPECTRE_DIR/2-init 2 && make && cd ~
+	cd $SPECTRE_DIR/ && make && cd ~
 fi
 
 # Launch the raw-status background program in hidden thread
--- rpi_config/launch_tag.sh
+++ rpi_config/launch_tag.sh
@@ -4,7 +4,7 @@ LASER_PROGRAM=$SPECTRE_DIR/laser/laser-brains
 
 # Ensure recent config files are stored correctly
 
-if [! -f $LASER_PROGRAM ]
+if [ ! -f $LASER_PROGRAM ]
 then
 	#Program does not exist. Re-run 'make'
 	cd $SPECTRE_DIR/laser && make && cd ~
